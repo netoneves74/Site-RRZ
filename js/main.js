6b1139e40eb17e384a5db300c21db8feb427c80d
Neto Neves <netonevesgbi@mail.com>
2018-09-04 17:37:40 -0300

    Criação da Barra de Pesquisa e Modificação do Carrocel

--- js/main.js
+++ js/main.js
@@ -4,3 +4,15 @@ $('.menu-anchor').on('click touchstart', function(e){
     $('.menu').toggleClass('menu-active');
     e.preventDefault();
 });
+
+$('.busca-anchor').on('click touchstart', function(e){
+    $('.campo-busca').toggleClass('busca-anchor-active');
+    $('.close-anchor').toggleClass('close-anchor-active');
+    e.preventDefault();
+});
+
+$('.close-anchor').on('click touchstart', function(e){
+    $(this).toggleClass('close-anchor-active');
+    $('.campo-busca').toggleClass('busca-anchor-active');
+    e.preventDefault();
+});
